29da7f3d1fd0aecdacd9aabb41f4aa22968f4ff9
Skvortsov <skvorzov.sv@gmail.com>
2022-01-27 00:10:50 +0200

    inital commit

---
+++ src/main/java/com/wtf/wtfproject/WtfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wtf.wtfproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtfProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtfProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wtf/wtfproject/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.wtf.wtfproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
